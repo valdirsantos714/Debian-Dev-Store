5c7a92fd0acdd4632646d2fc64ec91da6e2a9780
Valdir Santos <valdirsantost40@gmail.com>
2024-10-08 10:31:39 -0300

    feat: created more automations for the devops area

--- devops/devops.py
+++ devops/devops.py
@@ -3,9 +3,9 @@ import os
 class Devops:
 
     def __init__(self):
-        self.funcionalidades = ['1 - Instalar docker', "2 - Hello world Docker", "3 - ", "4 - ", "5 - Sair"]
+        self.funcionalidades = ['1 - Instalar docker', "2 - Instalar Ansible", "3 - Instalar Terraform", "4 - Executar Prometheus", "5 - Sair", "6 - Instalar Jenkins", "7 - Instalar Kubernates"]
 
-        self.executadores = ["", self.install_docker, self.hello_world_docker]
+        self.executadores = ["", self.install_docker, self.install_ansible, self.install_terraform, self.prometheus, "", self.install_jenkins, self.install_kubernates]
 
 
     def inicio(self):
@@ -44,12 +44,42 @@ class Devops:
         self.executadores[numero]()
         print(f"{self.executadores[numero]}")
 
-    def hello_world_docker(self):
-        os.system("sudo docker run hello-world")
-
     def install_docker(self):
         lista_comandos = ["curl -fsSL https://get.docker.com -o get-docker.sh", "sudo sh get-docker.sh"]
 
         for c in lista_comandos:
             os.system(c)
 
+    def install_ansible(self):
+        lista_comandos = ["sudo apt update", "sudo apt-get install -y software-properties-common", "sudo add-apt-repository --yes --update ppa:ansible/ansible", "sudo apt update", "sudo apt-get install ansible", "ansible --version"]
+
+        for c in lista_comandos:
+            os.system(c)
+
+    def install_terraform(self):
+        lista_comandos = ["wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg", "echo 'deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main' | sudo tee /etc/apt/sources.list.d/hashicorp.list", "sudo apt update && sudo apt install terraform"]
+
+        for c in lista_comandos:
+            os.system(c)
+
+    def prometheus(self):
+        lista_comandos = ["docker run -d -p 9090:9090 prom/prometheus"]
+
+        for c in lista_comandos:
+            os.system(c)
+    
+    def install_jenkins(self):
+        lista_comandos = ["sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key echo 'deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]' \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null", "sudo apt-get update", "sudo apt-get install jenkins"]
+        
+        for c in lista_comandos:
+            os.system(c)
+        
+    
+    def install_kubernates(self):
+        lista_comandos = ["curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg", "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list", "sudo apt update", "sudo apt install kubeadm kubelet kubectl", "sudo apt-mark hold kubeadm kubelet kubectl", "kubeadm version"]
+
+        for c in lista_comandos:
+            os.system(c)
--- software_development/developer.py
+++ software_development/developer.py
@@ -3,9 +3,9 @@ import os
 class Dev:
 
     def __init__(self):
-        self.funcionalidades = ['1 - Criar Projeto NodeJs', "2 - Criar projeto React Native + Typescript + Tailwind CSS ", "3 - Criar Projeto React", "4 - ", "5 - Sair"]
+        self.funcionalidades = ['1 - Criar Projeto NodeJs', "2 - Criar projeto React Native", "3 - Criar Projeto React", "4 - Criar Projeto Angular ", "5 - Sair"]
 
-        self.executadores = ["", self.nodejs_project, self.react_native_typescript, self.react_project]
+        self.executadores = ["", self.nodejs_project, self.react_native_project, self.react_project, self.angular_project]
 
 
     def inicio(self):
@@ -173,46 +173,75 @@ class Dev:
         print("Projeto criado com sucesso!")
 
 
-    def react_native_typescript(self):
+    def react_native_project(self):
 
-        caminho = "/home/valdir/Documentos/z_coisas_uteis"
-        #caminho_absoluto = input("Digite o caminho absoluto (ex: /home/user/Documentos/coisas_uteis) aonde vc deseja criar o seu projeto: ")
+        caminho_absoluto = input("Digite o caminho absoluto (ex: /home/user/Documentos/coisas_uteis) onde você deseja criar o seu projeto: ")
 
-        nome_projeto = input("Digite o nome do projeto que vc deseja criar : ")
-        os.system("cd") # cd
-        os.chdir(caminho) # Entra no caminho especificado
-        
-        lista_comandos = [f"npx create-expo-app {nome_projeto} --template expo-template-blank-typescript","npm i", "npm i nativewind", "npm i --save-dev tailwindcss@3.3.2", "npx tailwindcss init", """echo '
+        nome_projeto = input("Digite o nome do projeto que você deseja criar: ")
+
+        # Escolha de linguagem
+        print("Escolha a linguagem:")
+        print("1 - JavaScript")
+        print("2 - TypeScript")
+        linguagem = input("Digite o número da sua escolha: ")
+
+        # Pergunta se o usuário deseja usar NativeWind
+        usar_nativewind = input("Deseja usar NativeWind para estilização? (s/n): ")
+
+        # Base do comando para criar o projeto
+        if linguagem == "1":
+            comando_criar_projeto = f"npx create-expo-app {nome_projeto} --template expo-template-blank"
+        elif linguagem == "2":
+            comando_criar_projeto = f"npx create-expo-app {nome_projeto} --template expo-template-blank-typescript"
+        else:
+            print("Opção inválida.")
+
+        # Lista de comandos a serem executados
+        lista_comandos = [comando_criar_projeto, "npm i"]
+
+        if usar_nativewind.lower() == 's':
+            lista_comandos += [
+                "npm i nativewind",
+                "npm i --save-dev tailwindcss@3.3.2",
+                "npx tailwindcss init",
+                """echo '
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-+   plugins: ['nativewind/babel'],
+    plugins: ['nativewind/babel'],
   };
-};
-
-' > babel.config.js """, """echo '
+};' > babel.config.js""",
+                """echo '
 module.exports = {
-    - content: [],
-    + content: ['./src/**/*.{ts,tsx}'],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
-    }
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};' > tailwind.config.js"""
+            ]
 
-' > tailwind.config.js """, "npx expo start"]
+        # Executa os comandos
+        self.executar_comando(f"cd {caminho_absoluto}")
+        os.chdir(caminho_absoluto)
+        self.executar_comando(lista_comandos[0])
 
-        os.system(lista_comandos[0])
+        os.chdir(caminho_absoluto + f"/{nome_projeto}")
+        self.executar_comando(lista_comandos[1])
 
-        os.chdir(caminho + f"/{nome_projeto}")
+        if usar_nativewind.lower() == 's':
+            for i in (range(2, len(lista_comandos))):
+                self.executar_comando(lista_comandos[i])
 
-        for i in range(1, len(lista_comandos)):
-            os.system(lista_comandos[i])
+        print(f"Projeto React Native '{nome_projeto}' criado com sucesso!")
+
 
     def react_project(self): 
         
-        opcao = int(input("Deseja Criar Projeto React com:\n1-Javascript, 2-Typescript, 3-Javascript + React Router Dom, 4- Typescript + React Router Dom"))
+        print("Deseja Criar Projeto React com:")
+        print("\n1-Javascript, \n2-Typescript, \n3 - Javascript + React Router Dom, \n4 - Typescript + React Router Dom")
+        opcao = int(input("\nDigite a opção desejada: "))
 
         caminho_absoluto = input("Digite o caminho absoluto (ex: /home/user/Documentos/coisas_uteis) aonde vc deseja criar o seu projeto: \n")
 
@@ -262,4 +291,76 @@ module.exports = {
             for i in range(2,len(lista_comandos)):
                 os.system(lista_comandos[i])
         
+
+    def angular_project(self):
+        caminho_absoluto = input("Digite o caminho absoluto (ex: /home/user/Documentos/coisas_uteis) onde você deseja criar o seu projeto: ")
+
+        nome_projeto = input("Digite o nome do projeto que você deseja criar: ")
+
+        # Escolha de linguagem
+        print("Escolha a linguagem:")
+        print("1 - JavaScript")
+        print("2 - TypeScript (Recomendado)")
+        linguagem = input("Digite o número da sua escolha: ")
+
+        # Pergunta se o usuário deseja usar Tailwind CSS
+        usar_tailwind = input("Deseja usar Tailwind CSS para estilização? (s/n): ")
+
+        # Pergunta se deseja incluir Angular Material
+        incluir_material = input("Deseja incluir Angular Material? (s/n): ")
+
+        # Pergunta se deseja incluir RxJS
+        incluir_rxjs = input("Deseja incluir RxJS? (s/n): ")
+
+        # Base do comando para criar o projeto Angular
+        if linguagem == "1":
+            comando_criar_projeto = f"npx -p @angular/cli ng new {nome_projeto} --skip-install --style css --routing true --strict false"
+            os.chdir(caminho_absoluto)
+            self.executar_comando(comando_criar_projeto)
+            os.chdir(caminho_absoluto + f"/{nome_projeto}")
+            
+        elif linguagem == "2":
+            comando_criar_projeto = f"npx -p @angular/cli ng new {nome_projeto} --skip-install --style css --routing true --strict true"
+            os.chdir(caminho_absoluto)
+            self.executar_comando(comando_criar_projeto)
+            os.chdir(caminho_absoluto + f"/{nome_projeto}")
+           
+        else:
+            print("Opção inválida.")
+            return
+
+        # Lista de comandos a serem executados
+        lista_comandos = [comando_criar_projeto, f"cd {nome_projeto}", "npm install"]
+
+        # Comandos para instalar e configurar Tailwind CSS, se desejado
+        if usar_tailwind.lower() == 's':
+            lista_comandos += [
+                "npm install -D tailwindcss@latest postcss@latest autoprefixer@latest",
+                "npx tailwindcss init",
+                """echo 'module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};' > tailwind.config.js""",
+                """echo '@tailwind base;
+@tailwind components;
+@tailwind utilities;' >> src/styles.css"""
+            ]
+
+        # Comandos para incluir Angular Material, se desejado
+        if incluir_material.lower() == 's':
+            lista_comandos.append("ng add @angular/material")
+
+        # Comandos para incluir RxJS, se desejado
+        if incluir_rxjs.lower() == 's':
+            lista_comandos.append("npm install rxjs")
+
+        lista_comandos.append("ng serve --open")
+
+        for i in range(2,len(lista_comandos)):
+            self.executar_comando(lista_comandos[i])
+
+        print(f"Projeto Angular '{nome_projeto}' criado com sucesso!")
 
