8b01973feab5aca7bb390e0923bfc9d4686a80c0
Valdir Santos <valdirsantost40@gmail.com>
2024-10-08 11:59:44 -0300

    docs: add documentation to the project and exception handling

---
+++ README.md
@@ -0,0 +1,78 @@
+# Debian Dev Store
+
+## Descrição
+
+O **Debian Dev Store** é um repositório inovador desenvolvido em Python, projetado para automatizar a criação de projetos e a instalação de ferramentas essenciais para desenvolvedores. Este projeto visa resolver o problema da complexidade e da curva de aprendizado na configuração de ambientes de desenvolvimento, permitindo que os desenvolvedores se concentrem no que realmente importa: escrever código.
+
+### Objetivo
+
+Criado para atender à crescente demanda de desenvolvedores por soluções que facilitem a criação de projetos e a instalação de ferramentas, o Debian Dev Store oferece uma abordagem simplificada e eficiente. Com ele, você pode evitar a frustração de ter que consultar múltiplas documentações e escrever diversas linhas de comandos.
+
+## Estrutura do Projeto
+
+O Debian Dev Store é dividido em três áreas principais:
+
+1. **DevOps**
+   - Automatiza a instalação de ferramentas essenciais de DevOps como:
+     - **Ansible**: Automação de configuração e gerenciamento de sistemas.
+     - **Terraform**: Provisionamento de infraestrutura como código.
+     - **Kubernetes**: Orquestração de containers.
+     - **Prometheus**: Monitoramento e alertas de sistemas.
+
+2. **Software Development**
+   - Facilita a criação de projetos em diversas tecnologias, incluindo:
+     - **React**: Biblioteca JavaScript para criação de interfaces de usuário.
+     - **React Native**: Framework para desenvolvimento de aplicativos móveis.
+     - **Angular**: Plataforma para construção de aplicações web dinâmicas.
+     - **Node.js**: Ambiente de execução para JavaScript no lado do servidor.
+
+3. **Essential Tools**
+   - Automatiza a instalação de ferramentas fundamentais para o desenvolvimento, como:
+     - **Git**: Sistema de controle de versão.
+     - **Node**: Ambiente de execução para JavaScript.
+     - **Docker**: Plataforma para criar, implantar e executar aplicações em containers.
+     - **Terminator**: Terminal avançado para gerenciar múltiplas janelas.
+
+## Tecnologias Utilizadas
+
+- **Python 3**: A linguagem principal utilizada para o desenvolvimento do projeto.
+- **Comandos Debian**: Utilizados para a instalação e configuração de ferramentas e ambientes.
+
+## Compatibilidade
+
+O Debian Dev Store foi desenvolvido para funcionar exclusivamente em ambientes e plataformas Linux baseadas em Debian. Isso inclui distribuições como Ubuntu, Linux Mint e Debian, garantindo a melhor experiência ao usuário.
+
+## Funcionalidades
+
+- **Automação de Criação de Projetos**: Com um único comando, você pode criar projetos em diferentes tecnologias sem precisar se preocupar com a configuração manual.
+- **Instalação Simplificada de Ferramentas**: Instala ferramentas essenciais com facilidade, eliminando a necessidade de escrever longas sequências de comandos.
+- **Documentação Abrangente**: O repositório fornece documentação clara e concisa para ajudar os desenvolvedores a entenderem cada parte do processo de automação.
+- **Foco na Experiência do Usuário**: Todo o sistema foi projetado com a experiência do desenvolvedor em mente, visando facilitar o fluxo de trabalho e aumentar a produtividade.
+
+## Como Usar
+
+1. **Pré-requisitos**: Certifique-se de ter o Python 3 instalado em sua máquina.
+2. **Clone o Repositório**:
+   ```bash
+   git clone https://github.com/valdirsantos714/Debian-Dev-Store.git
+   cd Debian-Dev-Store
+   ```
+3. **Execute o Script de Automação**:
+     ```bash
+     python3 main.py
+     ```
+
+## Contribuições
+
+Contribuições são bem-vindas! Se você deseja ajudar a melhorar o Debian Dev Store, siga as etapas abaixo:
+
+1. Fork o repositório.
+2. Crie uma nova branch para suas alterações.
+3. Faça suas modificações e envie um pull request.
+
+## Contato
+
+Para dúvidas, sugestões ou feedback, entre em contato:
+
+- **E-mail**: valdirsantost40@gmail.com
+- **LinkedIn**: [Valdir Santos](https://www.linkedin.com/in/valdir-santos-8553002a4/)
--- essential_tools/essential_tools.py
+++ essential_tools/essential_tools.py
@@ -3,12 +3,26 @@ import os
 class EssentialTools:
     
     def __init__(self):
+        """
+        Inicializa a loja com uma lista de funcionalidades e de executadores.
+        """
+
         self.funcionalidades = ['1 - Instalar Git', "2 - Instalar Docker ", "3 - Instalar OpenSSH Server", "4 - Instalar VS Code", "5 - Sair", "6 - Instalar Terminator (Terminal Com autocomplete)", "7 - Instalar Node JS"]
 
         self.executadores = ["", self.install_git, self.install_docker, self.install_openssh, self.install_vscode, "", self.install_terminator, self.install_nodejs]
 
 
     def inicio(self):
+        """
+        Inicia o menu principal da loja, permitindo ao usuário escolher uma 
+        funcionalidade.
+
+        Esta função limpa a tela, exibe o cabeçalho da loja e apresenta 
+        as opções disponíveis. O usuário pode escolher uma opção 
+        digitando o número correspondente. A função continua 
+        executando até que o usuário escolha a opção para sair.
+        """
+
         os.system("clear")
         opcao = 0
 
@@ -33,11 +47,29 @@ class EssentialTools:
                 self.executa_funcoes(opcao)
 
     def executa_funcoes(self, numero):
+        """
+        Executa a função correspondente à opção escolhida pelo usuário.
+
+        Este método chama a função associada ao número fornecido, que 
+        representa uma das funcionalidades disponíveis. Após a execução, 
+        imprime o nome da função executada.
+
+        Parâmetros:
+        numero (int): O número da opção escolhida pelo usuário.
+        """
 
         self.executadores[numero]()
         print(f"{self.executadores[numero]}")
 
     def mostra_funcionalidade(self):
+        """
+        Exibe as funcionalidades disponíveis na loja.
+
+        Esta função imprime uma mensagem de boas-vindas e lista as opções disponíveis 
+        que o usuário pode escolher. Cada funcionalidade é mostrada em uma linha 
+        separada, permitindo que o usuário saiba quais ações podem ser realizadas.
+        """
+
         print("\nBem vindo a Loja Essential Tools!\n")
         print("Escolha uma das opções abaixo:")
 
@@ -45,38 +77,101 @@ class EssentialTools:
             print(f)   
 
     def install_git(self):
-        lista_comandos = ["sudo apt update", "sudo apt install git", "git --version"]
+        """
+        Instala o Git no sistema usando comandos de terminal.
 
-        for c in lista_comandos:
-            os.system(c)
+        Este método atualiza os pacotes do sistema, instala o Git e 
+        verifica a versão instalada.
+        """
+        try:
+            lista_comandos = ["sudo apt update", "sudo apt install git", "git --version"]
+
+            for c in lista_comandos:
+                os.system(c)
+
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+
 
     def install_docker(self):
-        lista_comandos = ["curl -fsSL https://get.docker.com -o get-docker.sh", "sudo sh get-docker.sh"]
+        """
+        Instala o Docker no sistema.
 
-        for c in lista_comandos:
-            os.system(c)
+        Este método baixa e executa o script de instalação do Docker.
+        """
+        try:
+            lista_comandos = ["curl -fsSL https://get.docker.com -o get-docker.sh", "sudo sh get-docker.sh"]
+
+            for c in lista_comandos:
+                os.system(c)
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+
     
     def install_openssh(self):
-        lista_comandos = ["sudo apt update", "sudo apt install openssh-server", "sudo systemctl status ssh", "sudo systemctl start ssh", "sudo systemctl enable ssh"]
+        """
+        Instala o OpenSSH Server no sistema.
 
-        for c in lista_comandos:
-            os.system(c)
+        Este método atualiza os pacotes do sistema, instala o OpenSSH, 
+        inicia o serviço SSH e o habilita para iniciar na inicialização. 
+        """
+        
+        try:
+            lista_comandos = ["sudo apt update", "sudo apt install openssh-server", "sudo systemctl status ssh", "sudo systemctl start ssh", "sudo systemctl enable ssh"]
+
+            for c in lista_comandos:
+                os.system(c)
+
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+
     
     def install_vscode(self):
-        lista_comandos = ["sudo apt update", "sudo apt install apt-transport-https curl", "curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/vscode.gpg", "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/code stable main' | sudo tee /etc/apt/sources.list.d/vscode.list", "sudo apt update", "sudo apt install code"]
+        """
+        Instala o Visual Studio Code no sistema.
 
-        for c in lista_comandos:
-            os.system(c)
+        Este método configura o repositório do VS Code e instala o editor.
+        """
+        try:
+            lista_comandos = ["sudo apt update", "sudo apt install apt-transport-https curl", "curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/vscode.gpg", "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/code stable main' | sudo tee /etc/apt/sources.list.d/vscode.list", "sudo apt update", "sudo apt install code"]
+
+            for c in lista_comandos:
+                os.system(c)
+                
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+
 
 
     def install_terminator(self):
-        lista_comandos = ["sudo apt-get install terminator", "sudo apt-get install zsh", "chsh -s $(which zsh)", "sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'", "sudo apt-get install fonts-powerline", "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions", "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git", "echo 'source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ${ZDOTDIR:-$HOME}/.zshrc", "sudo apt install git zsh zsh-autosuggestions zsh-syntax-highlighting fzf -y", "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k"]
+        """
+        Instala o Terminator, Zsh e configura o Oh My Zsh no sistema.
 
-        for c in lista_comandos:
-            os.system(c)
+        Este método instala o Terminator, o shell Zsh e algumas 
+        personalizações, incluindo plugins e temas.
+        """
+        try:
+            lista_comandos = ["sudo apt-get install terminator", "sudo apt-get install zsh", "chsh -s $(which zsh)", "sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'", "sudo apt-get install fonts-powerline", "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions", "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git", "echo 'source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ${ZDOTDIR:-$HOME}/.zshrc", "sudo apt install git zsh zsh-autosuggestions zsh-syntax-highlighting fzf -y", "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k"]
+
+            for c in lista_comandos:
+                os.system(c)
+
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+
     
     def install_nodejs(self):
-        lista_comandos = ["sudo apt update && sudo apt upgrade -y", "curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -", "sudo apt-get install -y nodejs", "node -v",  "npm -v"]
+        """
+        Instala o Node.js no sistema.
 
-        for c in lista_comandos:
-            os.system(c)
+        Este método atualiza o sistema, configura o repositório do Node.js 
+        e instala a versão LTS do Node.js e do NPM.
+        """
+        try:
+            lista_comandos = ["sudo apt update && sudo apt upgrade -y", "curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -", "sudo apt-get install -y nodejs", "node -v",  "npm -v"]
+
+            for c in lista_comandos:
+                os.system(c)
+
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
